639f97744b135f11a29b75c00f9bcdceef0aa22a
Joan Mendoza <wadalino@gmail.com>
2025-04-30 10:37:37 +0200

    adding method createPortal on Modal

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,13 +1,18 @@
+
+import ReactDOM from 'react-dom';
+
 // sniped 'rfc' and intro
 import './Modal.css';
 
+
+
 export default function Modal({children, handleTancar}) {
-  return (
+  return ReactDOM.createPortal((
     <div className="modal-fons">
         <div className="modal">
             {children}
             <button onClick={handleTancar}>Tancar</button>
         </div>
     </div>
-  )
+  ), document.body)
 }
