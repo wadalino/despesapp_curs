58b1b6379d197aae8513b715b9ad74213adaf387
Joan Mendoza <wadalino@gmail.com>
2025-04-30 11:37:39 +0200

    css by modules

--- src/components/despessesLlista/DespessesLlista.jsx
+++ src/components/despessesLlista/DespessesLlista.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
+import estils from './DespessesLlista.module.css'
 
 export default function DespessesLlista({despeses, handleClick}) {
   return (
-    <>
+    <div>
 {
     despeses.map((despesa, index)=>(
-      <React.Fragment key={despesa.id}>
+      <div key={despesa.id} className={estils.targeta}>
         <h2>{index+1} - {despesa.concepte}</h2>
         <button onClick={() => handleClick(despesa.id)}>Eliminar despesa</button>
-      </React.Fragment>
+      </div>
     ))
 }
-    </>
+    </div>
   )
 }
