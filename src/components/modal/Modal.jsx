48f4e6ac96e908ff8ca510f733b150e46cc9dc6f
Joan Mendoza <wadalino@gmail.com>
2025-04-30 09:57:56 +0200

    adding css to modal

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -2,6 +2,11 @@
 
 export default function Modal() {
   return (
-    <div>Modal</div>
+    <div className="modal-fons">
+        <div className="modal">
+            <h2>Component Modal</h2>
+            <p>Ara canviarem el contingut</p>
+        </div>
+    </div>
   )
 }
