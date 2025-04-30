b291ed046dcbde6e99b679ce3d98ad14b2c0d937
Joan Mendoza <wadalino@gmail.com>
2025-04-30 09:41:54 +0200

    remove div in Titol as using fragment

--- src/components/titol/Titol.jsx
+++ src/components/titol/Titol.jsx
@@ -3,10 +3,10 @@ import ('./Titol.css');
 export default function Titol({titol, subtitol}) {
 
     return (
-    <div>
-    <h1>{titol}</h1>
-    <br/>
-    <h2>{subtitol}</h2>
-    </div>
+    <>
+      <h1>{titol}</h1>
+      <br/>
+      <h2>{subtitol}</h2>
+    </>
   )
 }
