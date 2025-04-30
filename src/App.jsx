4d31d78c0d8563423585468301c384c2fa0a938b
Joan Mendoza <wadalino@gmail.com>
2025-04-30 09:28:14 +0200

    upload modifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,72 @@
+import { useState } from 'react';
 import './App.css';
 
+import Titol from './components/titol/Titol';
+
+
+// ESTATS
+
+
 function App() {
-  
-  const titol = "Benvinguts al Curs";
-  const subTitol = "React & Firebase";
-  const web = "http://www.menorca.info";
+  //let titol = "Benvinguts al Curs";
+  const [mostrarDespeses, setMostrarDespeses] = useState(true);
+  const subtitol = "Curs de React and Firebase";
+  const [despeses, setDespeses] = useState([
+    {id:1, concepte: "dinar", quantia: 22.3, pagatPer: "Joan"},
+    {id:2, concepte: "compra", quantia: 72.3, pagatPer: "Natalia"},
+    {id:3, concepte: "ordinador", quantia: 722.3, pagatPer: "Joan"},
+    {id:4, concepte: "chuches", quantia: 2.3, pagatPer: "Joan"},
+    {id:5, concepte: "llum", quantia: 52.3, pagatPer: "Natalia"},
+    {id:6, concepte: "hipoteca", quantia: 202.3, pagatPer: "Natalia"},
+  ]);
+
+  console.log(mostrarDespeses)
+
+  const handleClick = (id) => {
+    // console.log(id);
+    
+    // NOTE problema perque l'estat previ pot haver canviar 
+    // sempre s'ha de treballar amb un estat previ -> bones practiques de React
+    // FIXME 
+    // setDespeses( despeses.filter( (despesa) => despesa.id !== id )); 
+    setDespeses(
+      (despesesPrevies) => {
+        return despesesPrevies.filter( (despesa) => despesa.id !== id )
+      }
+    )
+
+  }
 
   return (
     <div>
-      <h2>{ titol }</h2>
-      <p>{ subTitol }</p>
-      <p>La data d'avui és: {new Date().toDateString() }</p>
-      <p>Número aleatòri: { Math.random() * 100 }</p>
-      <a href={ web }>Visita Es Diari</a>
+
+      <Titol titol="Benvinguts al curs" subtitol={subtitol} />
+
+      { !mostrarDespeses && (
+        <div>
+          <button onClick={() => setMostrarDespeses(true)}>Mostrar despeses</button>
+        </div>
+      )
+      }
+
+      { mostrarDespeses && (
+        <div>
+          <button onClick={() => setMostrarDespeses(false)}>Ocultar despeses</button>
+        </div>
+      )
+      }
+
+      { mostrarDespeses && 
+        despeses.map((despesa, index)=>(
+          <div key={despesa.id}>
+            <h2>{index+1} - {despesa.concepte}</h2>
+            <button onClick={() => handleClick(despesa.id)}>Eliminar despesa</button>
+          </div>
+        ))
+        
+      
+      }
+
     </div>
   )
 }
---
+++ src/components/titol/Titol.jsx
@@ -0,0 +1,12 @@
+import ('./Titol.css');
+
+export default function Titol({titol, subtitol}) {
+
+    return (
+    <div>
+    <h1>{titol}</h1>
+    <br/>
+    <h2>{subtitol}</h2>
+    </div>
+  )
+}
